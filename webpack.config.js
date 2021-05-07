329bfc5309933a7e6ab25ce9e5b5884d6c66639f
Anton <bufpost@yandex.ru>
2021-05-07 10:27:56 +0700

    Update dep

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,5 @@
-'use strict';
-
-var webpack = require('webpack');
-
-var plugins = [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-];
-
-if (process.env.NODE_ENV === 'production') {
-    plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                screw_ie8: true,
-                warnings: false
-            }
-        })
-    );
-}
-
 module.exports = {
+    mode: process.env.NODE_ENV || 'production',
     externals: {
         react: {
             root: 'React',
@@ -30,18 +9,14 @@ module.exports = {
         }
     },
     module: {
-        loaders: [{
+        rules: [{
             test: /\.js$/,
-            loaders: ['babel-loader'],
+            use: 'babel-loader',
             exclude: /node_modules/
         }]
     },
     output: {
         library: 'ReactCustomScrollbars',
         libraryTarget: 'umd'
-    },
-    plugins: plugins,
-    resolve: {
-        extensions: ['', '.js']
     }
 };
